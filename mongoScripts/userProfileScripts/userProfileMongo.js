1c342e9c731b139f4b34e85ae46755b4045c9c81
Jus <jssandh2@illinois.edu>
2015-12-13 21:10:12 -0500

    Just a temporary commit. Feel free to reset --hard on a commit number that excludes this file. I just need to keep track of it on master. - Juspreet

--- mongoScripts/userProfileScripts/userProfileMongo.js
+++ mongoScripts/userProfileScripts/userProfileMongo.js
@@ -3,3 +3,21 @@ import mongoose
 /*
 * Script to read new users from file, parse, create JSON -> dump into MongoDB user_profiles collection
 */
+
+var mongoose = require('mongoose');   // Extract the mongoose, and call constructor
+
+var db = mongoose.connection;     // Open up a connection with Mongo-DB
+
+db.on('error', console.error);
+db.once('open', function() {
+  // Creating the schema and the model for the user-Profile
+  var userProfile = new mongoose.Schema({
+    name : {type: String},
+    political_party : {type: String},
+    short_bio : {type : String},
+    
+  })
+
+})
+
+mongoose.connect('mongodb://localhost/test')
